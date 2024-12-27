06a32f1b8b2e34306c722acca4ca00b175c35bad
omkar1103 <115880514+omkar1103@users.noreply.github.com>
2024-12-27 17:44:51 +0530

    vite config edited

--- 04Bg_color/vite.config.js
+++ 04Bg_color/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ReactJs-Projects/',  // Replace with your repo name
+  base: '/ReactJs-Projects/04Bg_color/',  // Replace with your repo name
   build: {
     outDir: 'dist',  // This is the default output directory for Vite
   },
